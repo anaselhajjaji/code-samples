523d9edb7d039a2441db1ee80525e9d7ffba63c6
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-03-30 18:57:38 +0000

    saves the sample

---
+++ dotnet/csharp-perf-sqlite/Migrations/20220330184301_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharp_perf_sqlite.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MakeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MakeName = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedTimestamp = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
